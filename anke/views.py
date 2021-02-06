61778b7d4d6ad1c5c4d29c19c8eebd91808ef587
坂本祥太朗 <shochan1986@sakamotoshoutaakiranoMacBook-Air.local>
2021-02-06 20:39:11 +0900

    auth

--- anke/views.py
+++ anke/views.py
@@ -20,10 +20,10 @@ def ankeView(request):
     if request.method == 'POST':
         form = AnkeForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['name']
+            name = form.cleaned_data['name']
             from_email = form.cleaned_data['email']
-            message1 = (subject, f"「{subject}」さまがアンケートに回答されました。", from_email, ['s-shotaro@berraquera-jp.com'])
-            message2 = (subject, f"「{subject}」 さま \n ご回答ありがとうございます。\n 特設ページはこちらです→ https://kurumedmo-survey.herokuapp.com/gift/ \n ", from_email, [from_email])
+            message1 = (f"アンケート回答のお知らせ。「{name}」さま", f"「{name}」さまがアンケートに回答されました。", from_email, ['s-shotaro@berraquera-jp.com'])
+            message2 = ("アンケート回答特典クーポンについて", f"「{name}」 さま \n ご回答ありがとうございます。\n クーポンはこちらです→ https://kurumedmo-survey.herokuapp.com/gift/ \n\n リンクが開かない場合はスタッフにこのメールをお見せください。\n　クーポンの代わりとします。 ", from_email, [from_email])
             try:
                 send_mass_mail((message1, message2), fail_silently=False)
             except BadHeaderError:
@@ -64,7 +64,7 @@ def ankeExport(request):
     writer = csv.writer(response)
     writer.writerow(['回答日', 'ID', '手段', '氏名', '住所', 'Eメールアドレス', '質問１', '質問２', '質問３', '質問４', '質問５', '質問６', '質問7', '質問8', '質問9', '質問10', '質問11', '質問12', '質問13', '質問14', '質問15'])
     for answer in data:
-        writer.writerow([answer.created, answer.user, answer.status, answer.name, answer.address, answer.email, answer.question1, answer.question2, answer.question3, answer.question4.all(), answer.question5.all(), answer.question6.all(), answer.question7, answer.question8, answer.question9, answer.question10, answer.question11, answer.question12, answer.question13, answer.question14, answer.question15])
+        writer.writerow([answer.created.date(), answer.user, answer.status, answer.name, answer.address, answer.email, answer.question1, answer.question2, answer.question3, answer.question4.all(), answer.question5.all(), answer.question6.all(), answer.question7, answer.question8, answer.question9, answer.question10, answer.question11, answer.question12, answer.question13, answer.question14, answer.question15])
     return response
 
 
@@ -78,7 +78,7 @@ def ankeKapaExport(request):
     writer = csv.writer(response)
     writer.writerow(['回答日', 'ID', '手段', '氏名', '住所', 'Eメールアドレス', '質問１', '質問２', '質問３', '質問４', '質問５', '質問６', '質問7', '質問8', '質問9', '質問10', '質問11', '質問12', '質問13', '質問14', '質問15'])
     for answer in data:
-        writer.writerow([answer.created, answer.user, answer.status, answer.name, answer.address, answer.email, answer.question1, answer.question2, answer.question3, answer.question4.all(), answer.question5.all(), answer.question6.all(), answer.question7, answer.question8, answer.question9, answer.question10, answer.question11, answer.question12, answer.question13, answer.question14, answer.question15])
+        writer.writerow([answer.created.date(), answer.user, answer.status, answer.name, answer.address, answer.email, answer.question1, answer.question2, answer.question3, answer.question4.all(), answer.question5.all(), answer.question6.all(), answer.question7, answer.question8, answer.question9, answer.question10, answer.question11, answer.question12, answer.question13, answer.question14, answer.question15])
     return response
 
 @login_required
